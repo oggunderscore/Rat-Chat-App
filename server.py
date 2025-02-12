0156faac3dbf17b3802ce03366ee6f7e85685000
oggunderscore <oggunderscore@gmail.com>
2025-02-12 02:25:32 -0800

    Add TryCatch to ServerHandler

--- server.py
+++ server.py
@@ -8,13 +8,21 @@ clients = {}
 # Websocket handler
 async def handler(websocket):
     async for message in websocket:
-        device_name = await websocket.recv()
-        clients[websocket] = device_name  # Store device name
+        try: 
+            device_name = await websocket.recv()
+            # Store device name in the clients list
+            clients[websocket] = device_name  
+            async for message in websocket:
+                # Prefix messages with the sender's name
+                data = {"sender": device_name, "message": message}  
+                json_data = json.dumps(data)
 
-        async for message in websocket:
-            data = {"sender": device_name, "message": message}  # Prefix messages
-            json_data = json.dumps(data)
-
-            # Send the message to all connected clients
-            await asyncio.gather(*[client.send(json_data) for client in clients])
+                # Send the message to all connected clients
+                await asyncio.gather(*[client.send(json_data) for client in clients])
+        except websockets.exceptions.ConnectionClosed:
+            pass
+        finally:
+            # Remove client on disconnect
+            clients.pop(websocket, None)  
+            
             
