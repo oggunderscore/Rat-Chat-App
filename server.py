5d0832b9b5d87fd2649a8ea8a29d9c40a935beba
oggunderscore <oggunderscore@gmail.com>
2025-02-12 02:24:06 -0800

    Test code

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import asyncio
 import websockets
 import json
 
@@ -7,14 +8,13 @@ clients = {}
 # Websocket handler
 async def handler(websocket):
     async for message in websocket:
-        data = json.loads(message)
-        if data['type'] == 'register':
-            clients[data['id']] = websocket
-        elif data['type'] == 'message':
-            await clients[data['to']].send(json.dumps({
-                'type': 'message',
-                'from': data['from'],
-                'message': data['message']
-            }))
-            
+        device_name = await websocket.recv()
+        clients[websocket] = device_name  # Store device name
+
+        async for message in websocket:
+            data = {"sender": device_name, "message": message}  # Prefix messages
+            json_data = json.dumps(data)
+
+            # Send the message to all connected clients
+            await asyncio.gather(*[client.send(json_data) for client in clients])
             
